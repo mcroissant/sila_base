13e7c4d027f2f2b5fdf5e18d4487e9712382bce2
Ricardo Gaviria <ricardo.a.gaviria@gmail.com>
2019-02-25 14:32:51 +0100

    additional fixes, reverting xml changes

--- validate_features.sh
+++ validate_features.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find feature_definitions/ -type f -name "*.sila.xml" | xargs xmllint --schema schema/FeatureDefinition.xsd --noout
+find feature_definitions/ -type f -name "*.sila.xml" | xargs xmllint --schema schema/FeatureDefinition.xsd --schema schema/DataTypes.xsd --schema schema/Constraints.xsd --noout
