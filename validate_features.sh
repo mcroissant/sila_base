91bcc5cc4b88d7191c3be285b9b0b5cf62848f5c
Ricardo Gaviria <ricardo.a.gaviria@gmail.com>
2019-02-26 17:35:50 +0100

    Replace URL resource reference to relative path

--- validate_features.sh
+++ validate_features.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+SCHEMA_BASE_URL=https://gitlab.com/SiLA2/sila_base/raw/master/schema/
+grep -rl $(SCHEMA_BASE_URL) schema/ | xargs sed -i 's|'"$SCHEMA_BASE_URL"'||g'
 find feature_definitions/ -type f -name "*.sila.xml" | xargs xmllint --schema schema/FeatureDefinition.xsd --noout
