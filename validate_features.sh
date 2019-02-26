6fc41d3de1e35e8942060c9569d648cda13f4cdb
Ricardo Gaviria <ricardo.a.gaviria@gmail.com>
2019-02-26 17:54:09 +0100

    Update validate features to use local files

--- validate_features.sh
+++ validate_features.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 SCHEMA_BASE_URL=https://gitlab.com/SiLA2/sila_base/raw/master/schema/
-grep -rl $(SCHEMA_BASE_URL) schema/ | xargs sed -i 's|'"$SCHEMA_BASE_URL"'||g'
+LOCAL_SCHEMA_URI=file://$(pwd)/schema/
+
+# Replace schema location on XSD to relative paths
+grep -rnl "$SCHEMA_BASE_URL" . --include *.xsd | xargs sed -i 's|'"$SCHEMA_BASE_URL"'||g'
+
+# replace schema reference in XML's to local file
+grep -rnl "$SCHEMA_BASE_URL" . --include *.xml | xargs sed -i 's|'"$SCHEMA_BASE_URL"'|'"$LOCAL_SCHEMA_URI"'|g'
+
 find feature_definitions/ -type f -name "*.sila.xml" | xargs xmllint --schema schema/FeatureDefinition.xsd --noout
