5127860b6d4f2f209e3f0baf0c3f4be4eb1f5a75
Ricardo Gaviria <ricardo.a.gaviria@gmail.com>
2019-03-08 11:39:26 +0100

    Remove unecessary url replacements in validate_features.sh

--- validate_features.sh
+++ validate_features.sh
@@ -1,12 +1,2 @@
 #!/usr/bin/env bash
-SCHEMA_BASE_URL=https://gitlab.com/SiLA2/sila_base/raw/modular_schema/schema/
-SCHEMA_BASE_URL_MASTER=https://gitlab.com/SiLA2/sila_base/raw/master/schema/
-LOCAL_SCHEMA_URI=file://$(pwd)/schema/
-
-# Replace schema location on XSD to relative paths
-grep -rnl "$SCHEMA_BASE_URL" . --include *.xsd | xargs sed -i 's|'"$SCHEMA_BASE_URL"'||g'
-
-# replace schema reference in XML's to local file
-grep -rnl "$SCHEMA_BASE_URL_MASTER" . --include *.xml | xargs sed -i 's|'"$SCHEMA_BASE_URL_MASTER"'|'"$LOCAL_SCHEMA_URI"'|g'
-
 find feature_definitions/ -type f -name "*.sila.xml" | xargs xmllint --schema schema/FeatureDefinition.xsd --noout
