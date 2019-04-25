fab71da10d81aa680d1c4ab49d0cd534232651c4
Ricardo Gaviria <ricardo@unitelabs.ch>
2019-04-25 18:08:42 +0000

    Feature observable command controller

--- validate_features.sh
+++ validate_features.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
+INVALID_XML=`comm -23 <(find feature_definitions/ -type f -name "*.xml") <(find feature_definitions/ -type f -name "*.sila.xml")`
+
+if [ -z "$INVALID_XML" ]
+then
+      echo "No XML files found without SiLA Extension"
+else
+      printf '%s\n' "Error: Only feature definitions with '*.sila.xml' are allowed: $INVALID_XML" >&2
+      exit 1
+fi
+
 find feature_definitions/ -type f -name "*.sila.xml" | xargs xmllint --schema schema/FeatureDefinition.xsd --noout
