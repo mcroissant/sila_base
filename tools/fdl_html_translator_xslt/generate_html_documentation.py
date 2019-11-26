750599cb057de68f54b2c908a075c76a12c549fc
mark doerr <mark.doerr@uni-greifswald.de>
2019-11-26 15:17:15 +0100

    category page - nicer formatting

--- tools/fdl_html_translator_xslt/generate_html_documentation.py
+++ tools/fdl_html_translator_xslt/generate_html_documentation.py
@@ -102,7 +102,7 @@ def genIndexCategories(feature_path_dict, target_dir="."):
 
         for feature in feature_category_dict[feature_category] :
             logging.debug(f"---------> {feature}")
-            markdown_txt += f"[{feature.id}](md__docs_{feature.html_path}__{feature.id}.html)\n\n"
+            markdown_txt += f"  * [{feature.id}](md__docs_{feature.html_path}__{feature.id}.html)\n\n"
 
 
         #markdown_txt += f"[{feature}](md__docs_{feature_dict.html_path}__{convert(feature)}.html)\n\n"
