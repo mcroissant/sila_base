343d40a4682d701e2040b185ca76c9ceb311dbd7
Timothy Diguiet <timothy.diguiet@epitech.eu>
2020-04-29 03:57:56 +0200

    Remove: AutoCompleteSearch.tsx (added todo instead) Fix: Active tab in feature page Update: Observable.tsx to display observable or unobservable instead of icon

--- tools/documentation_app/src/components/FeaturesPage.tsx
+++ tools/documentation_app/src/components/FeaturesPage.tsx
@@ -2,13 +2,11 @@ import React, {ChangeEvent, Component} from "react";
 import {Tabs} from "@material-ui/core";
 import Tab from "@material-ui/core/Tab";
 import FeatureList, {FeatureMode} from "./FeatureList";
-import AutoCompleteSearch from "./AutoCompleteSearch";
 
 class FeaturesPage extends Component {
     state = {
         currentTab: FeatureMode.Core,
     }
-    value: FeatureMode = FeatureMode.Core;
 
     handleChange = (event: ChangeEvent<{}>, newValue: FeatureMode) => {
         this.setState({
@@ -16,16 +14,15 @@ class FeaturesPage extends Component {
         });
     };
 
-    // todo make active tab
     render(): React.ReactNode {
         return (
             <>
-                <Tabs value={this.value} onChange={this.handleChange} aria-label="simple tabs example">
+                <Tabs value={this.state.currentTab} onChange={this.handleChange} aria-label="simple tabs example">
                     <Tab label="Core Features" value={FeatureMode.Core}/>
                     <Tab label="All Features" value={FeatureMode.Alphabetical}/>
                     <Tab label="Features by category" value={FeatureMode.Category}/>
                 </Tabs>
-                <AutoCompleteSearch></AutoCompleteSearch>
+                {/* TODO add search bar to search features by name */}
                 <FeatureList mode={this.state.currentTab}/>
             </>
         );
--- tools/documentation_app/src/modules/features/components/Observable.tsx
+++ tools/documentation_app/src/modules/features/components/Observable.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Box from "@material-ui/core/Box/Box";
 import { IXmlToJsxProps, XmlToJsxTitle } from "../../xmlToJsxGen";
-import CheckIcon from "@material-ui/icons/Check";
-import ClearIcon from "@material-ui/icons/Clear";
+import {Typography} from "@material-ui/core";
 
 const Observable: React.FC<IXmlToJsxProps> = ({ content, style }) => {
     return (
         <Box p={1} style={style}>
-            <Box p={1} style={{ display: "inline-flex", alignItems: "center" }}>
+            <Box p={1} style={{ display: "inline-flex", alignItems: "bottom" }}>
                 <XmlToJsxTitle content={content} variant="h4" />
-                {content.textContent === "No" && <ClearIcon color="error" fontSize={"default"} />}
-                {content.textContent === "Yes" && <CheckIcon color="action" fontSize={"default"} />}
+                <Typography variant="body1" component="span" style={{fontWeight: 600, paddingLeft: "10px"}}>
+                    <p>
+                        {(content.textContent === "Yes") ? ("Observable") : ("Unobservable")}
+                    </p>
+                </Typography>
             </Box>
         </Box>
     );
